bc881a7411e4b213dcf8e95dbe4e76023a3753f6
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-20 15:25:37 +0700

    Fix code on main.tf

--- main.tf
+++ main.tf
@@ -13,51 +13,51 @@ provider "aws" {
   secret_key = var.aws_secretkey
 }
 
-resource "aws_vpc" "experimental_terraform" {
+resource "aws_vpc" "prometheus_grafana" {
   cidr_block = "10.10.0.0/16"
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
 
-resource "aws_subnet" "experimental_terraform_public" {
-  vpc_id                                      = aws_vpc.experimental_terraform.id
+resource "aws_subnet" "prometheus_grafana_public" {
+  vpc_id                                      = aws_vpc.prometheus_grafana.id
   cidr_block                                  = "10.10.10.0/24"
   map_public_ip_on_launch                     = true
   enable_resource_name_dns_a_record_on_launch = true
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
 
-resource "aws_internet_gateway" "experimental_terraform" {
-  vpc_id = aws_vpc.experimental_terraform.id
+resource "aws_internet_gateway" "prometheus_grafana" {
+  vpc_id = aws_vpc.prometheus.id
 
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
 
-resource "aws_route_table" "experimental_terraform_pubsub" {
-  vpc_id = aws_vpc.experimental_terraform.id
+resource "aws_route_table" "prometheus_grafana_pubsub" {
+  vpc_id = aws_vpc.prometheus_grafana.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.experimental_terraform.id
+    gateway_id = aws_internet_gateway.prometheus_grafana.id
   }
 
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
 
-resource "aws_route_table_association" "experimental_terraform_pubsub_routetable" {
-  subnet_id      = aws_subnet.experimental_terraform_public.id
-  route_table_id = aws_route_table.experimental_terraform_pubsub.id
+resource "aws_route_table_association" "prometheus_grafana_pubsub_routetable" {
+  subnet_id      = aws_subnet.prometheus_grafana_public.id
+  route_table_id = aws_route_table.prometheus_grafana_pubsub.id
 }
 
-resource "aws_security_group" "experimental_terraform_ec2" {
-  vpc_id = aws_vpc.experimental_terraform.id
+resource "aws_security_group" "prometheus_grafana_ec2" {
+  vpc_id = aws_vpc.prometheus_grafana.id
 
   ingress {
     description = "SSH"
@@ -92,34 +92,34 @@ resource "aws_security_group" "experimental_terraform_ec2" {
   }
 
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
 
-resource "aws_network_interface" "experimental_terraform_ec2_eth0" {
-  subnet_id       = aws_subnet.experimental_terraform_public.id
+resource "aws_network_interface" "prometheus_grafana_ec2_eth0" {
+  subnet_id       = aws_subnet.prometheus_grafana_public.id
   private_ips     = ["10.10.10.10"]
-  security_groups = [aws_security_group.experimental_terraform_ec2.id]
+  security_groups = [aws_security_group.prometheus_grafana_ec2.id]
   tags = {
-    Name = "experimental_terraform_ec2_eth0"
+    Name = "prometheus_grafana_ec2_eth0"
   }
 }
 
-resource "aws_instance" "experimental_terraform" {
+resource "aws_instance" "prometheus_grafana" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.experimental_terraform_ec2_eth0.id
+    network_interface_id = aws_network_interface.prometheus_grafana_ec2_eth0.id
     device_index         = 0
   }
 
-  user_data = file("nginx_install.sh")
+  user_data = file("prometheus_install.sh")
   root_block_device {
     delete_on_termination = true
   }
 
   tags = {
-    Name = "experimental_terraform"
+    Name = "prometheus_grafana"
   }
 }
